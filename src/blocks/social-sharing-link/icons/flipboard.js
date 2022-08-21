b6a9f303f5f1d52c76efa6cd4bfd9b31382a6ba6
Nick Diego <ndiego@outermost.co>
2022-08-21 07:11:14 -0500

    Update and simplify SVG icons.

--- src/blocks/social-sharing-link/icons/flipboard.js
+++ src/blocks/social-sharing-link/icons/flipboard.js
@@ -5,6 +5,6 @@ import { Polygon, SVG } from '@wordpress/primitives';
 
 export const FlipboardIcon = () => (
 	<SVG width="24" height="24" viewBox="0 0 24 24" version="1.1">
-		<Polygon points="9.33333333 20 4 20 4 4 20 4 20 9.33333333 14.6666667 9.33333333 14.6666667 14.6666667 9.33333333 14.6666667" />
+		<Polygon points="9.333 20 4 20 4 4 20 4 20 9.333 14.667 9.333 14.667 14.667 9.333 14.667" />
 	</SVG>
 );

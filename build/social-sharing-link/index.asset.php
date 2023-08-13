5b9e6a1e6cb42893a7f19c014ca74c728e1e159e
Nick Diego <nickmdiego@gmail.com>
2023-08-13 10:01:13 -0500

    Fresh build.

--- build/social-sharing-link/index.asset.php
+++ build/social-sharing-link/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '104551a79e946f80d4db');
+<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '59382961caeeca25ff7c');
--- build/social-sharing/index.asset.php
+++ build/social-sharing/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-keycodes', 'wp-primitives', 'wp-url'), 'version' => 'f1e9ddcd5c99d50094dc');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-keycodes', 'wp-primitives', 'wp-url'), 'version' => '23a23f578a42da63fe14');

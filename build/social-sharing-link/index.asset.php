d11ac9a66979986dec44bb29c3f64507683d3a52
Watermark Branding <151574669+WatermarkBranding@users.noreply.github.com>
2024-02-28 13:37:03 -0800

    Added support for Threads share
    Committing changes to src/ and build/, as well as updates to package-lock.json and readme. <VERSION> and <DATE> need to be updated appropriately in readme.txt.

--- build/social-sharing-link/index.asset.php
+++ build/social-sharing-link/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'f1b0c1058d711139dffc');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n', 'wp-primitives'), 'version' => 'c1ac1c4f59872a6de0d1');

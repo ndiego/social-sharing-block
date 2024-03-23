d628652ae46aaee11f1f9755181303604dc2de3b
Nick Diego <nickmdiego@gmail.com>
2024-03-23 09:29:03 -0500

    Fresh build and removed .map files.

--- build/social-sharing-link/index.asset.php
+++ build/social-sharing-link/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n', 'wp-primitives'), 'version' => 'c1ac1c4f59872a6de0d1');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'abe7142db3892bd5c19b');

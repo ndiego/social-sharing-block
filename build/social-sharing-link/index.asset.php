8e70c46cfdde0b1ef71060c70d18de49328f93f0
Alexandre REVIRE <alexandrer@bsa-web.fr>
2024-10-10 11:36:52 +0200

    chores: run build

--- build/social-sharing-link/index.asset.php
+++ build/social-sharing-link/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n', 'wp-primitives'), 'version' => '80d27eea6c4beeab694b');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n', 'wp-primitives'), 'version' => 'd621166fe77dc5fd218e');

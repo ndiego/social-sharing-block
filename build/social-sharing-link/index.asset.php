44ae35c23bd982aa6bb1c2c469d00f7d2de9385d
Nick Diego <nickmdiego@gmail.com>
2024-11-02 14:29:25 -0500

    Fresh build.

--- build/social-sharing-link/index.asset.php
+++ build/social-sharing-link/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n', 'wp-primitives'), 'version' => 'd621166fe77dc5fd218e');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n', 'wp-primitives'), 'version' => 'b0091f8bf14d9fbd4ea6');
